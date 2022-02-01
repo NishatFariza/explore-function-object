d803a91f021cfb5d79f162521a7ed9043ffcab6e
NishatFariza <nishatfarzia78@gmail.com>
2022-02-01 16:19:18 +0600

    simple object practice

---
+++ object.js
@@ -0,0 +1,20 @@
+var student1={
+    idNumber: 113,
+    name: 'Abir Adnan',
+    marks: 81,
+    phone: 000000,
+}
+
+var mobile={
+    color :'Black',
+    price :19000,
+    screenSize: 5,
+    stroage: '16gb',
+}
+var book ={
+    title: 'Start With Why',
+    author: 'Simon Sinek',
+    price: 220,
+    pages: 320,
+    publsher: 'penguin',
+}
