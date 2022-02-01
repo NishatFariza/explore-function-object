d8039bb273176c607ad34525be1b92c154055c94
NishatFariza <nishatfarzia78@gmail.com>
2022-02-01 15:45:35 +0600

    function practice

---
+++ multiple-parameter.js
@@ -0,0 +1,45 @@
+// function addTwoNumber(number1, number2){
+//     console.log(number1, number2);
+//     var totalNumber=number1+number2;
+//     return totalNumber;
+// }
+// var firstNumber=35;
+// var secondNumber=65;
+// var result =addTwoNumber(secondNumber, firstNumber);
+// console.log (result);
+
+
+//mutiplication in function
+// function multiplyTwoNumbers(num1, num2){
+//     var result = num1 * num2;
+//     return result;
+
+// }
+// var firstNumber=100;
+// var secondNumber=3;
+// var total = multiplyTwoNumbers(firstNumber, secondNumber);
+// console.log('Total multiplication Number', total);
+
+
+// substraction in function 
+// function substractionTwoNumber(number1, number2){
+//     var result =number1 - number2;
+//     return result;
+
+// }
+// var firstNumber=200;
+// var secondNumber=50;
+// var totalNumber= substractionTwoNumber(firstNumber, secondNumber);
+// console.log('This is total substraction Number', totalNumber);
+
+// divided in function 
+function dividedTwoNumber(number1, number2){
+    var result =number1 / number2;
+      return result;
+
+
+}
+var firstNumber = 300;
+var secondNumber =3;
+var totalNumber =dividedTwoNumber(firstNumber, secondNumber);
+console.log('Divided result is', totalNumber);
