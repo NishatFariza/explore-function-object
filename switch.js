0fe453dbbb995a6bb843bfd39a006ae34f4babe0
NishatFariza <nishatfarzia78@gmail.com>
2022-02-01 18:11:19 +0600

    switch practice

---
+++ switch.js
@@ -0,0 +1,53 @@
+var color='blue';
+
+// if(color== 'red'){
+//     console.log('This color is red');
+// }
+// else if(color== 'blue'){
+//     console.log('This color is blue');
+// }
+// else if(color== 'yellow'){
+//     console.log('This color is yellow');
+// }
+// else if(color== 'brown'){
+//     console.log('This color is brown');
+// }
+// else if(color== 'magenta'){
+//     console.log('This color is magenta');
+// }
+// else if(color== 'purple'){
+//     console.log('This color is purple');
+// }
+// else if(color== 'white'){
+//     console.log('This color is white');
+// }
+// else{
+//     console.log('This color is black');
+// }
+
+
+switch (color) {
+    case 'red':
+      console.log('This is red');
+      break;
+    case 'blue':
+      console.log('This is blue');
+      break;
+    case 'yellow':
+      console.log('This is yellow');
+      break;
+    case 'purple':
+      console.log('This is purple');
+      break;
+    case 'green':
+      console.log('This is green');
+      break;
+    case 'magenta':
+      console.log('This is magenta');
+      break;
+    case 'white':
+      console.log('This is white');
+      break;
+      default:
+          console.log('This is Black');
+}
